4bcf60a2fa0a44048eb78acd67a116c205c245d6
domenicolafroscia <domenicolafroscia@gmail.com>
2024-02-01 14:39:33 +0100

    add: index method in api controller

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index() {
+        $projects = Project::with('technologies')->paginate(10);
+        return response()->json([
+            'result' => $projects,
+            'success' => true,
+        ]);
+    }
+}
