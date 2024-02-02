3fa08e2be8d678fe1367d060f49a499a8477f669
domenicolafroscia <domenicolafroscia@gmail.com>
2024-02-02 17:00:01 +0100

    add: API creation via show function

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -8,11 +8,29 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $projects = Project::with('technologies')->paginate(10);
         return response()->json([
             'result' => $projects,
             'success' => true,
         ]);
+    }
+
+    public function show($slug)
+    {
+        $project = Project::with('type', 'technologies')->where('slug', $slug)->first();
+
+        if ($project) {
+            return response()->json([
+                'result' => $project,
+                'success' => true,
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'No project',
+            ]);
+        }
     }
 }
